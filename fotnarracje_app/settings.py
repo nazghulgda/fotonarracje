c5019a0d933229fb0d9336583945fe923bc26ea7
Jarek Klamann <jarek.klamann@gmail.com>
2022-08-23 10:24:00 +0200

    gitignore updated, database added in settings

--- fotnarracje_app/settings.py
+++ fotnarracje_app/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'fotnarracje_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'NAME': 'foto_narracje_database',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'fotograf',
+        'PASSWORD': 'fotonarracje',
     }
 }
 
